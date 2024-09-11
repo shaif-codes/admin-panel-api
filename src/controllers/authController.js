14d996444d84e72a5a45481fc9eba0db46cf87ba
shaif-codes <91661068+shaif-codes@users.noreply.github.com>
2024-09-11 07:01:05 +0530

    Refactor authController.js and add audit log functionality

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -3,12 +3,14 @@ const Role = require('../models/Role');
 const bcrypt = require('bcrypt');
 // const { Op, Sequelize } = require('sequelize');
 const { generateToken } = require('../utils/jwtUtils.js');
+const { createAuditLog } = require('./auditLogController');
 
 const signup = async (req, res) => {
     try {
         // Check if there's already an admin user
         const existingAdmin = await User.findOne({ where: { roleId: 'admin' } });
         if (existingAdmin) {
+            await createAuditLog('CREATE_ADMIN', req.user.id, 'Admin user already exists', 'CREATE_ADMIN');
             return res.status(400).json({ error: 'Admin user already exists' });
         }
 
@@ -30,8 +32,10 @@ const signup = async (req, res) => {
             user: newUser,
             token
         });
+        await createAuditLog('CREATE_ADMIN', newUser.id, `Admin user with ID: ${newUser.id} created`, `${newUser.id}`);
     } catch (error) {
         res.status(500).json({ error: 'Error creating admin user', message: error.message });
+        await createAuditLog('CREATE_ADMIN', req.user.id, `Error creating admin user:\n ${error.message}`, 'CREATE_ADMIN');
     }
 };
 const registerUser = async (req, res) => {
@@ -39,6 +43,10 @@ const registerUser = async (req, res) => {
         const { username, email, password, roleType } = req.body;
         const hashedPassword = await bcrypt.hash(password, 10);
         const role = await Role.findOne({ where: { name: roleType } });
+        if(!role){
+            await createAuditLog('CREATE_USER', req.user.id, `Role with name: ${roleType} not found`, `${req.user.id}`);
+            return res.status(404).json({ error: 'Role not found' });
+        }
         const newUser = await User.create({
             username,
             email,
@@ -50,8 +58,10 @@ const registerUser = async (req, res) => {
             message: 'User registered successfully',
             user: newUser
         });
+        await createAuditLog('CREATE_USER', newUser.id, `User with ID: ${newUser.id} created`, `${newUser.id}`);
     } catch (error) {
         res.status(500).json({ error: 'Error registering user', message: error.message });
+        await createAuditLog('CREATE_USER', req.user.id, `Error creating user:\n ${error.message}`, `${req.user.id}`);
     }
 };
 
@@ -62,11 +72,13 @@ const login = async (req, res) => {
         const user = await User.findOne({ where: { username } });
         // console.log('user :', user);
         if (!user) {
+            // await createAuditLog('LOGIN', null, 'Invalid credentials', 'LOGIN');
             return res.status(401).json({ error: 'Invalid credentials' });
         }
 
         const isMatch = await bcrypt.compare(password, user.password);
         if (!isMatch) {
+            await createAuditLog('LOGIN', user.id, 'Invalid credentials', 'LOGIN');
             return res.status(401).json({ error: 'Invalid credentials' });
         }
         const role = await Role.findOne({ where: { id: user.roleId } });
@@ -81,8 +93,10 @@ const login = async (req, res) => {
             message: 'Login successful',
             token
         });
+        await createAuditLog('LOGIN', user.id, 'Login successful', 'LOGIN');
     } catch (error) {
         res.status(500).json({ error: 'Error logging in', message: error.message });
+        // await createAuditLog('LOGIN', null, `Error logging in:\n ${error.message}`, 'LOGIN');
     }
 };
 
